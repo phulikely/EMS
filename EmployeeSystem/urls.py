93bb0de200ea5ade82c5a79df2374a1da5507e0a
Phulikely <phulikely@gmail.com>
2021-03-25 11:22:11 +0700

    done backend of 3 forms : index, login, registration

--- EmployeeSystem/urls.py
+++ EmployeeSystem/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from loginapp import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- loginapp/views.py
+++ loginapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponseRedirect
 from .models import User
 import mysql.connector
 from operator import itemgetter
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def index(req):
     return render(req, 'index.html')
@@ -10,27 +11,33 @@ def index(req):
 def login(req):
     return render(req, 'login.html')
 
-def home(req):
+def welcome(req):
     if req.method == "POST":
-        user = User()
-        if user.objects.get(email=req.POST['email'], password=req.POST['password']):
-            return render(req, 'welcome', {'user': user})
+        if User.objects.filter(email=req.POST['email'], password=req.POST['password']).exists():
+            user = User.objects.get(email=req.POST['email'], password=req.POST['password'])
+            return render(req, 'welcome.html', {'user': user})
+            #return redirect('welcome', {'user': user})
         else:
             context = {'msg': 'Invalid email or password!'}
-            return render(req, 'login', context)
+            return render(req, 'login.html', context)
             
 
 def registration(req):
     if req.method =="POST":
+        # Check email exists or not
+        if User.objects.filter(email=req.POST['email']).exists():
+            context = {'msg': 'Email already existed!'}
+            return render(req, 'registration.html', context)
+        # Check password matching        
+        elif req.POST['pwd'] != req.POST['repwd']:
+            context = {'msg': 'Passwords do not match!'}
+            return render(req, 'registration.html', context)           
         user = User(fname = req.POST['fname'],
                     lname = req.POST['lname'],
                     email = req.POST['email'],
-                    password = "123",
+                    password = req.POST['pwd'],
                     )
         user.save()
         return redirect('login')
     else:
         return render(req, 'registration.html')
-
-def welcome(req):
-    return render(req, 'welcome.html')
